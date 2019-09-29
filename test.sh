6b5985df4e37bd45bb50dd2dcda80b9c7014f561
Antonin Delpeuch <antonin@delpeuch.eu>
2019-09-29 11:07:44 +0200

    Test JSON schemas and examples in Travis. Closes #6

---
+++ test.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e
+
+# Test JSON examples against schemas
+
+for schema_file in `ls schemas`; do
+        echo "Testing $schema_file"
+        schema_name=$(echo "$schema_file" | cut -f 1 -d '.')
+        ajv test -s schemas/$schema_file -d "examples/$schema_name/valid/*.json" --valid
+        ajv test -s schemas/$schema_file -d "examples/$schema_name/invalid/*.json" --invalid
+done
