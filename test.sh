662553460be3b8218c536578e1f0f28108b3b207
Antonin Delpeuch <antonin@delpeuch.eu>
2019-12-14 18:13:45 +0100

    Fix test.sh after versioning

--- test.sh
+++ test.sh
@@ -4,15 +4,15 @@ set -e
 
 # Test JSON examples against schemas
 for version in `echo latest`; do
-	cd $version
+	echo "Testing version: $version"
+
 	for schema_file in `ls schemas`; do
 		echo "Testing $schema_file"
 		schema_name=$(echo "$schema_file" | cut -f 1 -d '.')
-		ajv test -s schemas/$schema_file -d "examples/$schema_name/valid/*.json" --valid
-		ajv test -s schemas/$schema_file -d "examples/$schema_name/invalid/*.json" --invalid
+		ajv test -s $version/schemas/$schema_file -d "$version/examples/$schema_name/valid/*.json" --valid
+		ajv test -s $version/schemas/$schema_file -d "$version/examples/$schema_name/invalid/*.json" --invalid
 		echo ""
 	done
-	cd ..
 done
 
 if [ $? -eq 0 ]
