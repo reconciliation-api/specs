2fd1bd8d9b3954e6c3bc1ba855a8c227d0b3ab6a
Antonin Delpeuch <antonin@delpeuch.eu>
2019-12-14 21:09:05 +0100

    Update respec and fix test.sh

--- test.sh
+++ test.sh
@@ -6,7 +6,7 @@ set -e
 for version in `echo latest`; do
 	echo "Testing version: $version"
 
-	for schema_file in `ls schemas`; do
+	for schema_file in `ls $version/schemas`; do
 		echo "Testing $schema_file"
 		schema_name=$(echo "$schema_file" | cut -f 1 -d '.')
 		ajv test -s $version/schemas/$schema_file -d "$version/examples/$schema_name/valid/*.json" --valid
