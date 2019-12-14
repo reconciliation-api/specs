8fe8db9785490bfec9f01850a2699b7f77a8c386
Antonin Delpeuch <antonin@delpeuch.eu>
2019-12-14 18:09:58 +0100

    Versioning, closes #7

--- test.sh
+++ test.sh
@@ -3,13 +3,16 @@
 set -e
 
 # Test JSON examples against schemas
-
-for schema_file in `ls schemas`; do
-        echo "Testing $schema_file"
-        schema_name=$(echo "$schema_file" | cut -f 1 -d '.')
-        ajv test -s schemas/$schema_file -d "examples/$schema_name/valid/*.json" --valid
-        ajv test -s schemas/$schema_file -d "examples/$schema_name/invalid/*.json" --invalid
-        echo ""
+for version in `echo latest`; do
+	cd $version
+	for schema_file in `ls schemas`; do
+		echo "Testing $schema_file"
+		schema_name=$(echo "$schema_file" | cut -f 1 -d '.')
+		ajv test -s schemas/$schema_file -d "examples/$schema_name/valid/*.json" --valid
+		ajv test -s schemas/$schema_file -d "examples/$schema_name/invalid/*.json" --invalid
+		echo ""
+	done
+	cd ..
 done
 
 if [ $? -eq 0 ]
